78041aaa0a711bd541e6bd82d338d822bc16b482
hencilpeter <38835386+hencilpeter@users.noreply.github.com>
2023-10-01 00:25:40 +0800

    additional changes.

--- project1_EnterpriseAnalyticalDataLake/source_code/staging_data_processor/validator_main.py
+++ project1_EnterpriseAnalyticalDataLake/source_code/staging_data_processor/validator_main.py
@@ -1,5 +1,5 @@
 from util.landing_files_processor import LandingFilesProcessor
 
 if __name__ == '__main__':
-    landing_files_processor = LandingFilesProcessor(landing_path="E:\\temp\\20230918\\")
+    landing_files_processor = LandingFilesProcessor(landing_path="/home/itv007175/data/landing/", hdfs_path='hdfs://m01.itversity.com:9000/user/itv007175/datalake/stage/' ,as_of_date='20230927')
     landing_files_processor.process()
